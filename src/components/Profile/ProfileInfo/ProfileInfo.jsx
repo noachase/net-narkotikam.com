ff0429e2b6e5a62ccf17fa32d06c843e15355616
Panacea <noach4se@gmail.com>
2020-06-27 16:00:34 +0200

    deleted comments

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,29 +2,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from '../../../Common/Preloader/Preloader';
 
-// class ProfileInfo extends React.Component {
-// 	componentDidMount() {
-// 		let s = this.props;
-// 		if (!this.props.profile) {
-// 			return <Preloader/>
-// 		}
-// 	}
-// 	render() {
-// 		return (
-// 			<div>
-// 				{/* <div>
-// 					<img alt="banner" src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
-// 				</div> */}
-// 				<div className={classes.description_block}>
-// 					{/* <img className={classes.ava} alt="avatar" src={this.props.profile.photos.large} /> */}
 
-// 				</div>
-// 				<h3 className="name">Eugene Markelov</h3>
-
-// 			</div>
-// 		)
-// 	}
-// }
 
 const ProfileInfo = (props) => {
 	if (!props.profile) {
